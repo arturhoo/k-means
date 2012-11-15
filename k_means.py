f0b1cb5b3a6e07fa4ee72e5f77fe4ddfb9f798bc
Artur Rodrigues <arturhoo@gmail.com>
2012-11-14 22:41:34 -0200

    define a Music and its associated point

--- k_means.py
+++ k_means.py
@@ -2,6 +2,17 @@
 from sys import exit, exc_info
 
 
+class Music(object):
+    """docstring for Music"""
+    def __init__(self, id, list_of_tags):
+        self.list_of_tags = list_of_tags
+
+    def set_associated_point(self, list_of_all_tags):
+        self.associated_point = [1 if tag in self.list_of_tags
+                                 else 0
+                                 for tag in list_of_all_tags]
+
+
 def get_content_from_file(file_name):
     try:
         with open(file_name) as f:
@@ -25,6 +36,17 @@ def get_set_of_tags_from_file(file_name):
     return tags
 
 
+def get_associated_point(list_of_tags, observation):
+    return [1 if tag in observation else 0 for tag in list_of_tags]
+
+
 if __name__ == '__main__':
-    tags = get_set_of_tags_from_file('data/lfm.dat')
-    print len(tags)
+    file_name = 'data/lfm.dat'
+    musics = []
+    tags = get_set_of_tags_from_file(file_name)
+    list_of_all_tags = sorted(tags)
+    for line in get_content_from_file(file_name):
+        list_from_line = line.strip().split()
+        music = Music(list_from_line[0], list_from_line[1:])
+        music.set_associated_point(list_of_all_tags)
+        musics.append(music)
