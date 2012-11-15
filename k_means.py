d3c2c1c86b71cfc01af7fe5de56b4ec76911aa40
Artur Rodrigues <arturhoo@gmail.com>
2012-11-15 00:45:46 -0200

    k-means working

--- k_means.py
+++ k_means.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 from utils import get_content_from_file
+from random import randint, random
+from pprint import pprint
 
 
 class Point(object):
+    # maybe leave the constructor empty and create a method for building the
+    # vector from base and instace lists
     def __init__(self, base_list=None, instance_list=None):
         if base_list is not None and instance_list is not None:
             self.vector = [1 if item in instance_list
@@ -17,8 +21,8 @@ class Point(object):
 
 
 class Music(object):
-    """docstring for Music"""
     def __init__(self, id, list_of_tags):
+        self.id = id
         self.list_of_tags = list_of_tags
         self.point = None
 
@@ -41,11 +45,55 @@ def get_associated_point(list_of_tags, observation):
 
 if __name__ == '__main__':
     file_name = 'data/lfm.dat'
+    k = 5
     musics = []
     tags = get_set_of_tags_from_file(file_name)
     list_of_all_tags = sorted(tags)
+
+    print 'Numbers of tags:', len(list_of_all_tags)
+
     for line in get_content_from_file(file_name):
         list_from_line = line.strip().split()
         music = Music(list_from_line[0], list_from_line[1:])
         music.set_point(list_of_all_tags)
         musics.append(music)
+
+    # rand_bin_list = lambda n: [randint(0, 1) for b in range(1, n + 1)]
+    rand_bin_list = lambda n: [random() for b in range(1, n + 1)]
+    clusters = [rand_bin_list(len(list_of_all_tags)) for i in range(k)]
+
+    last_matches = None
+    for t in range(100):
+        print 'Iteration', t
+        best_matches = [[] for i in range(k)]
+
+        # find which centroid is the closest for each music
+        for music in musics:
+            best_match = -1
+            best_match_distance = float("inf")
+            for i, cluster in enumerate(clusters):
+                cluster_point = Point()
+                cluster_point.vector = cluster
+                d = music.point.euclidean_distance(cluster_point)
+                if d < best_match_distance:
+                    best_match = i
+                    best_match_distance = d
+            best_matches[best_match].append(music)
+
+        # if the results are the same as last time, this is complete
+        if best_matches == last_matches:
+            break
+        last_matches = best_matches
+
+        # move the centroids to the average of their members
+        print 'Moving the centroids'
+        for i in range(k):
+            averages = [0.0] * len(list_of_all_tags)
+            if len(best_matches[i]) > 0:
+                list_of_points = [music.point.vector for music in best_matches[i]]
+                sum_of_points = [sum(value) for value in zip(*list_of_points)]
+                averages = [float(ssum) / float(len(list_of_points)) for ssum in sum_of_points]
+            clusters[i] = averages
+
+        for i, match in enumerate(best_matches):
+            print i, len(match)
