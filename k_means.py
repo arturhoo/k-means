52e94aa6bcd56782fc4c5788b62aa29ea87d20b5
Artur Rodrigues <arturhoo@gmail.com>
2012-11-14 22:58:15 -0200

    introduce points and euclidean distance

--- k_means.py
+++ k_means.py
@@ -2,15 +2,28 @@
 from sys import exit, exc_info
 
 
+class Point(object):
+    def __init__(self, base_list=None, instance_list=None):
+        if base_list is not None and instance_list is not None:
+            self.vector = [1 if item in instance_list
+                           else 0
+                           for item in base_list]
+        else:
+            self.vector = []
+
+    def euclidean_distance(self, point):
+        squares = [(x - y) ** 2 for x, y in zip(self.vector, point.vector)]
+        return sum(squares)
+
+
 class Music(object):
     """docstring for Music"""
     def __init__(self, id, list_of_tags):
         self.list_of_tags = list_of_tags
+        self.point = None
 
-    def set_associated_point(self, list_of_all_tags):
-        self.associated_point = [1 if tag in self.list_of_tags
-                                 else 0
-                                 for tag in list_of_all_tags]
+    def set_point(self, list_of_all_tags):
+        self.point = Point(list_of_all_tags, self.list_of_tags)
 
 
 def get_content_from_file(file_name):
@@ -48,5 +61,5 @@ if __name__ == '__main__':
     for line in get_content_from_file(file_name):
         list_from_line = line.strip().split()
         music = Music(list_from_line[0], list_from_line[1:])
-        music.set_associated_point(list_of_all_tags)
+        music.set_point(list_of_all_tags)
         musics.append(music)
